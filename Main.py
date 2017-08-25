a537ceab7b94e6a43122a3b3f5aa153141b57531
Dorian Rosen <dorianrrosen@gmail.com>
2017-08-25 15:05:07 -0600

    update

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ from Player import *
 pygame.init()
 screen = pygame.display.set_mode((1024, 768))
 pygame.display.set_caption("Destructo-Spin!")
+clock = pygame.time.Clock()
+FPS = 60
 
 player = Player("TempPlayer.png")
 
@@ -14,5 +16,7 @@ while True:
              sys.exit()
 
     player.update()
+    screen.fill((0, 0, 0))
     player.draw(screen)
+    clock.tick(FPS)
     pygame.display.flip()
--- Player.py
+++ Player.py
@@ -1,6 +1,14 @@
 import pygame, math
 from Vector import *
 
+def rotateCenter(image, angle):
+    orig_rect = image.get_rect()
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
+
 class Player:
     def __init__(self, filename):
 
@@ -18,13 +26,14 @@ class Player:
 
 
     def update(self):
-        self.rotationalSpeed *= .95
+        #self.rotationalSpeed *= .5
         keys = pygame.key.get_pressed()
         #if keys[pygame.K_SPACE]:
             #self.rect
         if keys[pygame.K_LSHIFT]:
-            if self.rotationalSpeed == 0:
-                self.rotationalSpeed =+ .01
-            self.rotationalSpeed *= 1.2
-        self.image = pygame.transform.rotate(self.image, (self.rotationalSpeed*(10 * (math.pi/180))))
+            self.image = rotateCenter(self.image, (5 * (math.pi / 180)))
 
+            #if self.rotationalSpeed == 0:
+            #    self.rotationalSpeed =+ .001
+            #self.rotationalSpeed *= 1.2
+        #self.image = rotateCenter(self.image, (self.rotationalSpeed*(10*(math.pi/180))))
