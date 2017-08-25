201e7b5998f4be2491133b353fd4b18f5200c1dc
Dorian Rosen <dorianrrosen@gmail.com>
2017-08-25 15:20:13 -0600

    update

--- Player.py
+++ Player.py
@@ -7,12 +7,18 @@ def rotateCenter(image, angle):
     rot_rect = orig_rect.copy()
     rot_rect.center = rot_image.get_rect().center
     rot_image = rot_image.subsurface(rot_rect).copy()
-    return rot_image
+    #return rot_image
+
+    loc = image.get_rect().center  #rot_image is not defined
+    rot_sprite = pygame.transform.rotate(image, angle)
+    rot_sprite.get_rect().center = loc
+    return rot_sprite
 
 class Player:
     def __init__(self, filename):
 
         self.rotationalSpeed = 0
+        self.lateralSpeed = 0
         self.location = (512-32, 384-32)
         self.direction = Vector(0, 1)
         self.image = pygame.image.load(filename)
@@ -21,17 +27,18 @@ class Player:
 
 
     def draw(self, screen):
-        screen.blit(self.image, self.location)
+        screen.blit(self.image, self.rect)
 
 
 
     def update(self):
         #self.rotationalSpeed *= .5
         keys = pygame.key.get_pressed()
-        #if keys[pygame.K_SPACE]:
-            #self.rect
         if keys[pygame.K_LSHIFT]:
-            self.image = rotateCenter(self.image, (5 * (math.pi / 180)))
+            self.lateralSpeed += 1
+            self.rect.center = ((self.rect.centerx+(self.lateralSpeed*self.direction.x)), (self.rect.centery+(self.lateralSpeed*self.direction.y)))
+        #if keys[pygame.K_SPACE]:
+            #self.image = rotateCenter(self.image, (5 * (math.pi / 180)))
 
             #if self.rotationalSpeed == 0:
             #    self.rotationalSpeed =+ .001
