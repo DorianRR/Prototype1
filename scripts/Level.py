944c00eecb8c23cc1560f48d39222eb682df7e53
Dorian Rosen <dorianrrosen@gmail.com>
2017-08-31 09:48:14 -0600

    Revert "Revert "Merge branch 'master' of https://github.com/DorianRR/Prototype1""
    This reverts commit d338c1ac880177d9aa39d4b49ebd990c5c9a5436.

--- scripts/Level.py
+++ scripts/Level.py
@@ -9,6 +9,7 @@ player = Player("PlayerCharacterTemp.png")
 class Level:
     def __init__(self):
         box = DestructibleObject("BoxCollider", (100, 100))
+<<<<<<< HEAD
 class Level:
     def __init__(self, map):
         """
@@ -16,24 +17,29 @@ class Level:
         the map start in the appropriate place, and then use the temp list
         to move the map when the player presses left shift.
         """
+=======
+>>>>>>> 4c5817def192865c9fc2d80c2db237b0f19c7033
         self.count = 0
         self.temp = []
-
-
         self.lateralSpeed = 0
         self.direction = Vector(0, -1)
+
         self.temp_X = 0
         self.temp_Y = 0
 
+        sqrt2 = math.sqrt(2) / 2
+        self.rotationList = [(0, -1), (sqrt2, -sqrt2), (1, 0), (sqrt2, sqrt2), (0, 1), (-sqrt2, sqrt2), (-1, 0),
+                             (-sqrt2, -sqrt2)]
+        self.rotateCounter = 0
+        self.fuelLevel = 3000
+        self.imageRotated = 0
         self.kickCounter = 0
         self.MoneyDamage = 0
         self.cameraOffsetX = 0
         self.cameraOffsetY = 0
+        self.count = 0
+        self.temp = []
 
-
-        """
-        Timer information used to make the player spool up their rotation
-        """
         self.delayTimer = 0
         self.delay = 6
 
@@ -41,14 +47,9 @@ class Level:
         self.collidableSprites.add(box)
         self.map = pygame.image.load("BgForTest.png").convert()
 
-
-    """
-    Shift level is called in main and moves the screen with the player. Will also
-    be used to move all other colliding objectrs. Called each frame.
-    """
     def shiftLevel(self):
         if self.count == 0:
-            self.temp = [512, 380]
+            self.temp = [544, 352]
             self.temp[0] -= 512
             self.temp[1] -= 384
             self.count = 1
@@ -61,29 +62,30 @@ class Level:
             item.rect.y -= self.cameraOffsetY
         return self.temp
 
+<<<<<<< HEAD
     def shift(self)
         return self.temp
+=======
+    def shiftWorld(self):
+        pass
+>>>>>>> 4c5817def192865c9fc2d80c2db237b0f19c7033
 
     def draw(self, screen):
         screen.blit(self.map, self.shiftLevel())
         self.collidableSprites.draw(screen)
 
-    """
-    Logic that makes the player kick off and move, reads in direction from the  player
-    class. Camera offset is set in the update method and that information will be
-    used to move all of the colliding objects.
-    """
     def update(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LSHIFT] and self.kickCounter <= 0:
+        if keys[pygame.K_LCTRL] and self.kickCounter <= 0:
             self.kickCounter = 30
             self.lateralSpeed += 12
             self.temp_X = player.direction.x
             self.temp_Y = player.direction.y
-            self.cameraOffsetX = (self.lateralSpeed * self.temp_X)
-            self.cameraOffsetY = (self.lateralSpeed * self.temp_Y)
         if self.lateralSpeed < 1.5:
             self.lateralSpeed = 0
+
+        self.cameraOffsetX = (self.lateralSpeed * self.temp_X)
+        self.cameraOffsetY = (self.lateralSpeed * self.temp_Y)
 
         self.kickCounter -= 1
         self.lateralSpeed *= .95
