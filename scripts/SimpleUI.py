8c95ef0d5f20717f7359eb5b63b028c9d91f5008
miiicom <miiicom1230@gmail.com>
2017-09-06 13:25:02 -0600

    update

--- scripts/SimpleUI.py
+++ scripts/SimpleUI.py
@@ -4,12 +4,13 @@ from Level import *
 
 def uicreate(Level):
     #create a Font object from the system fonts
-    scoreFont = pygame.font.SysFont('comicsansms',32,True)
-    numberFont = pygame.font.SysFont('comicsansms', 125, True)
+    newFont = pygame.font.Font("../font/Old_School_Adventures.ttf",20)
+    newFont1 = pygame.font.Font("../font/Old_School_Adventures.ttf", 60)
 
-    scoreSurface = scoreFont.render((u'Property Damage:'+ '$'), True, [255, 20, 20], None)
-    numberSurface = numberFont.render(str(Level.MoneyDamage),True,[255,20,20],None) #render(text, antialias, color, background=None) -> Surface
+
+    scoreSurface = newFont.render((u'Property Damage:'+ '$'), False, [255, 20, 20], None)
+    numberSurface = newFont1.render(str(Level.MoneyDamage),False,[255,20,20],None) #render(text, antialias, color, background=None) -> Surface
                                                                 #draw text on a new Surface, creates a new Surface with the specified text rendered on it
-    return [numberSurface,scoreSurface]
+    return [scoreSurface,numberSurface]
 
 
